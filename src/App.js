53741d1b795b5ec05c7079ea2c14c3275f151b3a
bkarabo754 <bkmasango26@gmail.com>
2022-12-12 11:57:45 +0200

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
   const [role, setRole] = useState();
+  const [employees, setEmployees] = useState([
+    {
+      name: "Bongani", 
+      role: 'Software Developer', 
+      img: 'https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg'
+    },
+    {
+      name: "Chris", 
+      role: 'Manager', 
+      img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg '
+    },
+    {
+      name: "Timothy", 
+      role: 'Intern', 
+      img: 'https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg'
+    },
+    {
+      name: "Mike", 
+      role: 'Junior Developer', 
+      img: 'https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg'
+    },
+    {
+      name: "John", 
+      role: 'Senior Executive Producer', 
+      img: 'https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg'
+    },
+    {
+      name: "Steve", 
+      role: 'Technician', 
+      img: 'https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg'
+    }
+  ]);
+
   const showEmployees = true;
   return (
     <div className="App">
-      {showEmployees ?
+      {showEmployees ? (
       <>
       <input
         type="text"
@@ -17,11 +51,15 @@ function App() {
           setRole(e.target.value)
         }} 
         />
-      <Employee name="Bongani" role="Software Developer"/>
-      <Employee name="Karabo" role={role}/>
-      <Employee name="Masego" />
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {
+            return (
+            <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img}/>
+            )
+          })}
+        </div>
       </>
-      : <p>You cannot see the employees</p>
+    )  :  <p>You cannot see the employees</p>
     }
       </div>
   );
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,25 @@
-function Employee ({name, role}) {
+import EditEmployee from "./EditEmployee";
+
+function Employee ({name, role, img}) {
     return (
-        <>
-        <h3>Employee: {name}</h3>
-        <p>{role ? role : "No role"}</p>
-        </>
+        <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={img} 
+        />
+     <div class="text-center space-y-2 sm:text-left">
+     <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold">
+        {name}
+      </p>
+      <p class="text-slate-500 font-medium">
+        {role} 
+      </p>
+    </div>
+
+    <EditEmployee />
+
+    
+  </div>
+</div>
     )
 }
 
